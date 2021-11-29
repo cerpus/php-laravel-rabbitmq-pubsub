b5d44ccea0d7f613e6af219f32304e96a8e87c2f
Max Moeschinger <m.moeschinger@gmail.com>
2021-11-29 14:44:01 +0100

    Remove obsolete method

--- src/Facades/RabbitMQPubSub.php
+++ src/Facades/RabbitMQPubSub.php
@@ -3,17 +3,11 @@
 namespace Cerpus\LaravelRabbitMQPubSub\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\Facades\Log;
 
 class RabbitMQPubSub extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
         return 'rabbitMQPubSub';
-    }
-
-    public function publish(string $topic, $data)
-    {
-        Log::info("Publish message to $topic");
     }
 }
