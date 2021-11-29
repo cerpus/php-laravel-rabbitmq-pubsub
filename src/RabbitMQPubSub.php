ee11047484f5678bf1d091b2550c4fae3f966459
Max Moeschinger <m.moeschinger@gmail.com>
2021-11-29 14:37:09 +0100

    Setup package

--- src/RabbitMQPubSub.php
+++ src/RabbitMQPubSub.php
@@ -2,18 +2,44 @@
 
 namespace Cerpus\LaravelRabbitMQPubSub;
 
-use Illuminate\Support\Facades\Facade;
+use Exception;
 use Illuminate\Support\Facades\Log;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
-class RabbitMQPubSub extends Facade
+class RabbitMQPubSub
 {
-    protected static function getFacadeAccessor(): string
+    private AMQPStreamConnection $connection;
+    private array $channels = [];
+
+    /**
+     * @throws Exception
+     */
+    public function __construct()
     {
-        return 'rabbitMQPubSub';
+        $this->connection = new AMQPStreamConnection(
+            config('rabbitMQPubSub.connection.host'),
+            config('rabbitMQPubSub.connection.port'),
+            config('rabbitMQPubSub.connection.user'),
+            config('rabbitMQPubSub.connection.password')
+        );
     }
 
-    public function publish(string $topic, $data)
+    private function getTopicChannel(string $topicName): AMQPChannel
     {
-        Log::info("Publish message to $topic");
+        if (!array_key_exists($topicName, $this->channels)) {
+            $channel = $this->connection->channel();
+            $channel->exchange_declare($topicName, 'fanout', false, true, false);
+            $this->channels[$topicName] = $channel;
+        }
+
+        return $this->channels[$topicName];
+    }
+
+    public function publish(string $topicName, string $data)
+    {
+        Log::info("Publish message to $topicName with data $data");
+        $this->getTopicChannel($topicName)->basic_publish(new AMQPMessage($data));
     }
 }
